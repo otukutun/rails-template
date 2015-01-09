bd321b11732a48b6a407906d05c9470fcda6a70e
otukutun <orehaorz@gmail.com>
2015-01-09 14:26:11 +0900

    add gem

--- template.rb
+++ template.rb
@@ -1,15 +1,29 @@
 # Gemfile
-gem_group :test, :development do
+# implements API
+gem 'grape'
+gem 'grape-jbuilder'
+gem 'mysql2'
+gem 'settingslogic'
+
+gem 'draper', :github => 'drapergem/draper'
+
+gem_group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
   gem 'simplecov' if yes?('use simplecov ?')
 end
 
+# rspec initalize setting
+run 'bundle install'
+run 'rm -rf test'
+generate 'rspec:install'
+
+# rm unused files
 run "rm README.rdoc"
 
+# git initalize setting
 after_bundle do
   git :init
   git add: "."
   git commit: %Q{ -m 'Initial commit' }
 end
-
