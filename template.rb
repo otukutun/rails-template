d129e71a9e25b91e6fc57ed263d4fd682543a90d
otukutun <orehaorz@gmail.com>
2015-01-16 00:03:48 +0900

    fix gem

--- template.rb
+++ template.rb
@@ -1,25 +1,31 @@
 # Gemfile
 
 # authentication
-gem 'devise' if yes?('use devise ?')
-gem 'sorcery' if yes?('use sorcery ?')
+if yes?('use devise ?')
+  gem 'devise'
+  generate 'devise:install'
+  rake 'db:migrate'
+end
 
-# redis
+if yes?('use sorcery ?')
+  gem 'sorcery'
+  generate 'sorcery:install'
+  rake 'db:migrate'
+end
+
+gem 'mysql2'
+gem 'slim-rails'
+gem 'settingslogic' # environment variables
+gem 'draper', :github => 'drapergem/draper'
 gem 'redis'
-gem 'redis-rails'
 
-# file uploader
 gem 'carrierwave'
 gem 'rmagick'
-
-# for s3
 gem 'fog'
 
-# api versioning
-gem 'versionist'
+gem 'versionist' # api versioning
 
-# pagination
-gem 'kaminari'
+gem 'kaminari' # pagination
 
 # grape 
 if yes?('use grape ?')
@@ -27,10 +33,6 @@ if yes?('use grape ?')
   gem 'grape-jbuilder'
 end
 
-gem 'mysql2'
-gem 'settingslogic'
-
-gem 'draper', :github => 'drapergem/draper'
 
 gem_group :development, :test do
   gem 'rspec-rails'
@@ -42,6 +44,10 @@ end
 gem_group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
+end
+
+gem_group :production do
+  gem 'unicorn'
 end
 
 # rspec initalize setting
